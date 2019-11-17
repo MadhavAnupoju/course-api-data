318b03b6b3b96ea0ecc850b67412e71d984ce25b
dell <anupoju.madhav@gmail.com>
2019-11-17 11:12:13 +0530

    Removed link

--- course-api-data/src/main/java/com/course/api/app/CourseApiDataApplication.java
+++ course-api-data/src/main/java/com/course/api/app/CourseApiDataApplication.java
@@ -2,7 +2,6 @@ package com.course.api.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CourseApiDataApplication {
@@ -10,5 +9,4 @@ public class CourseApiDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiDataApplication.class, args);
 	}
-//https://javabrains.io/courses/spring_bootquickstart/lessons/Making-Crud-Operations-with-Repository/
 }
